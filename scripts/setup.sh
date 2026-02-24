14e90e2282abab24db9841ae550a44d53572fdd9
Gary Burnett <gburnett@nvidia.com>
2026-02-24 10:11:25 -0800

    fix: import submodules

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash 
 
-# Install additional dependencies 
+# Install dependencies 
 sudo apt install -y bcftools tree pigz
+
+# Import submodules (not properly imported by brev)
+git submodule init
+git submodule update --recursive --remote
 
 # Pull Docker containers to avoid cluttering the notebook output 
 docker pull nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1
