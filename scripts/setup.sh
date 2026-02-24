813b70bcd2b32fefecfcbd12a03056e08172411f
Gary Burnett <gburnett@nvidia.com>
2026-02-24 09:14:10 -0800

    feat: adding pigz for faster decompression

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 # Install additional dependencies 
-sudo apt install -y bcftools tree 
+sudo apt install -y bcftools tree pigz
 
 # Pull Docker containers to avoid cluttering the notebook output 
 docker pull nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1
