2d237f730853ab91d137bc99cdd3f65d504e5bad
Gary Burnett <gburnett@nvidia.com>
2026-02-24 13:07:03 -0800

    fix: moving setup script final steps to beginning of notebook until better solution

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,13 +3,14 @@
 # Install apt dependencies 
 sudo apt install -y bcftools tree pigz tabix
 
+REPO_PATH=/home/ubuntu/genomics-analysis
+
 # Import submodules (not properly imported by brev)
-git submodule init
-git submodule update --recursive --remote
+cd ${REPO_PATH} && git submodule update --init --recursive
 
 # Install Python dependencies
 curl -sSL https://bootstrap.pypa.io/get-pip.py | python3
-python3 -m pip install -r CodonFM/requirements.txt
+python3 -m pip install -r ${REPO_PATH}/CodonFM/requirements.txt
 
 # Pull Docker containers to avoid cluttering the notebook output 
 docker pull nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1
