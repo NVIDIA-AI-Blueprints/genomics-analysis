4135d708d87d2cf1ba4861d997288943c139ea3b
Gary Burnett <gburnett@nvidia.com>
2026-02-24 08:23:52 -0800

    fix: updating setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,9 +3,5 @@
 # Install additional dependencies 
 sudo apt install -y bcftools tree 
 
-# For the CodonFM Submodule (not properly loaded into brev)
-# 3. Switch to the main branch 
-cd CodonFM && git switch main 
-
 # Pull Docker containers to avoid cluttering the notebook output 
 docker pull nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1
