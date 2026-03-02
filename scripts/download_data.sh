7d36a7cc6f509e12991750d8a9d65b79db5c3faf
Gary Burnett <gburnett@nvidia.com>
2026-03-02 11:17:38 -0800

    fix: incorporating changes from QA

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -3,30 +3,18 @@
 
 #!/bin/bash 
 
-# If the data directory already exists then exit 
-DATA_DIR="$PWD/data"
-if [ -d $DATA_DIR ]; then
-  echo "Data directory already exists, exiting."
-  exit
-fi
-
-# Make directories for data, references, and output 
+# Make directories for data and references files 
+DATA_DIR=$(pwd)/data
 mkdir -p $DATA_DIR/ref
-mkdir -p $DATA_DIR/output
 
 # Download the exome files 
 cd $DATA_DIR && \
-    wget ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R1_001.fastq.gz && \
-    wget ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz && \
+    wget -nc --progress=bar:force 2>&1 ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R1_001.fastq.gz && \
+    wget -nc --progress=bar:force 2>&1 ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz && \
     cd - 
 
-# Download and untar the Parabricks sample data
-wget -O parabricks_sample.tar.gz https://s3.amazonaws.com/parabricks.sample/parabricks_sample.tar.gz
-tar xzvf parabricks_sample.tar.gz
-
-# We only need the reference files from this
-mv parabricks_sample/Ref parabricks_sample/ref
-mv parabricks_sample/ref $DATA_DIR
-
-# Delete the leftover datafiles and tarball 
-rm -rf parabricks_sample*
+# Download the reference files 
+# Note: This downloads unnecessary files and can be streamlined in the future 
+wget -nc --progress=bar:force 2>&1 -O parabricks_sample.tar.gz https://s3.amazonaws.com/parabricks.sample/parabricks_sample.tar.gz
+pigz -dc parabricks_sample.tar.gz | tar xf -
+mv parabricks_sample/Ref $DATA_DIR/ref
