4cc590b4e778d9569d56fd6749dbce5387374d8d
Gary Burnett <gburnett@nvidia.com>
2026-03-02 09:27:43 -0800

    fix: setup script properly installs python packages

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash 
 
 # Install apt dependencies 
-sudo apt install -y bcftools tree pigz tabix
+sudo apt install -y bcftools pigz tabix
 
-REPO_PATH=/home/ubuntu/genomics-analysis
+# Each cloud provider uses a different home dir 
+REPO_PATH=/home/$(whoami)/genomics-analysis
+PYTHON_PATH=/home/$(whoami)/.venv/bin/python3
 
-# Import submodules (not properly imported by brev)
+# Import submodules 
 cd ${REPO_PATH} && git submodule update --init --recursive
 
 # Install Python dependencies
-curl -sSL https://bootstrap.pypa.io/get-pip.py | python3
-python3 -m pip install -r ${REPO_PATH}/CodonFM/requirements.txt
+curl -sSL https://bootstrap.pypa.io/get-pip.py | ${PYTHON_PATH}
+${PYTHON_PATH} -m pip install -r ${REPO_PATH}/CodonFM/requirements.txt
 
 # Pull Docker containers to avoid cluttering the notebook output 
 docker pull nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1
